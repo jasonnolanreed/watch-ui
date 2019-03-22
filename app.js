b2e8cf8a0bf1da6934c1cb45688c000d9666a279
Nolan Reed <jasonnolanreed@nolans-mbp.attlocal.net>
2019-03-22 11:00:07 -0400

    Hopefully enable ga pageviews

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ import './router.js';
 
 window[`ga`] = window[`ga`] || (_ => null);
 
+ga(`create`, `UA-136438475-1`, `auto`);
 // ga('send', 'event', 'Video (category)`, 'play (action)', 'cats.mp4 (label)');
