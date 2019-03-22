2761bada5bf96d4c454f0efb314effbace5c36fe
Nolan Reed <jasonnolanreed@nolans-mbp.attlocal.net>
2019-03-22 12:19:50 -0400

    Update GA class to store sends before tracker is ready

--- ga.js
+++ ga.js
@@ -1,17 +1,32 @@
 export class GA {
 	constructor() {
+		GA.send = GA.send.bind(this);
 		this.hasCreatedDefaultTracker = false;
+		this.storedSends = [];
 	}
 
 	static send(...options) {
-		if (!(`ga` in window)) { return; }
+		if (!(`ga` in window)) {
+			this.storedSends.push(options);
+			return;
+		}
 		if (!this.hasCreatedDefaultTracker) {
 			this.hasCreatedDefaultTracker = true;
 			ga(`create`, `UA-136438475-1`, `auto`);
 		}
-		if (typeof ga.getAll !== `function`) { return; }
+		if (typeof ga.getAll !== `function`) {
+			this.storedSends.push(options);
+			return;
+		}
 		const tracker = ga.getAll()[0];
-		if (!tracker) { return; }
+		if (!tracker) {
+			this.storedSends.push(options);
+			return;
+		}
+		if (this.storedSends.length) {
+			this.storedSends.forEach(options => tracker.send(...options));
+			this.storedSends = [];
+		}
 		tracker.send(...options);
 	}
 }
