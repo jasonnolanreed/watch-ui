b8cf6b887d123ee5d1ad2919799d83e21cee0536
Nolan Reed <jasonnolanreed@nolans-mbp.attlocal.net>
2019-03-22 13:14:22 -0400

    Have I been using the wrong ga script??

--- ga.js
+++ ga.js
@@ -1,21 +1,5 @@
 export class GA {
-	constructor() {
-		GA.view = GA.view.bind(this);
-		this.hasCreatedDefaultTracker = false;
-		this.storedViews = [];
-	}
-
 	static view(url) {
-		if (!(`ga` in window)) {
-			this.storedViews.push(url);
-			return;
-		}
-		if (!this.hasCreatedDefaultTracker) {
-			this.hasCreatedDefaultTracker = true;
-			ga(`create`, `UA-136438475-1`, `auto`);
-		}
-		this.storedViews.forEach(url => ga(`set`, `page`, url));
-		this.storedViews = [];
 		ga(`set`, `page`, url);
 		ga(`send`, `pageview`);
 	}
