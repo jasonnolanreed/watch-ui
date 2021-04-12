fc6968f1c8be406157e4e43e740f09207c17f473
Nolan Reed <jasonnolanreed@gmail.com>
2021-04-12 15:48:12 -0400

    More detailed /stats page

--- components/stats/stats-templates.js
+++ components/stats/stats-templates.js
@@ -1,9 +1,26 @@
+import {Format} from '../../utilities/date-time.js';
+
 export const makeTemplate = (component) => (
 `
 <h1><gwbw-icon name="insert_chart"></gwbw-icon> Stats</h1>
-<div><b>Users:</b> ${component.stats.users}</div>
-<div><b>Pre Users:</b> ${component.stats.preusers}</div>
-<div><b>Watches:</b> ${component.stats.watches}</div>
-<div><b>Measures:</b> ${component.stats.measures}</div>
+<div><b>Users:</b> ${component.stats.usersCount}</div>
+<div><b>Pre Users:</b> ${component.stats.preusersCount}</div>
+<div><b>Watches:</b> ${component.stats.watchesCount}</div>
+<div><b>Measures:</b> ${component.stats.measuresCount}</div>
+
+<h1><gwbw-icon name="insert_chart"></gwbw-icon> Last ${component.stats.latestMeasures.length} Measures</h1>
+${outputLatestMeasures(component.stats.latestMeasures)}
 `
 );
+
+const outputLatestMeasures = latestMeasures => {
+	let html = ``;
+	latestMeasures.map(measure => {
+		html += `
+			<div><b>${Format.dateAndTime(measure.date)}:</b> ${measure.watch}</div>
+			<div>${measure.user}</div>
+			<hr/>
+		`;
+	});
+	return html;
+};
