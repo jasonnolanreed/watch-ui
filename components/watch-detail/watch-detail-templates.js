e85487489d0f8264302f5a241a85fef737e6d3e4
Nolan Reed <jasonnolanreed@gmail.com>
2022-03-10 13:37:06 -0600

    Reduce vertical padding on interval icon

--- components/watch-detail/watch-detail-templates.js
+++ components/watch-detail/watch-detail-templates.js
@@ -218,7 +218,7 @@ const makeCss = (component) => (
 	font-size: 1.65em;
 	color: var(--blue);
 	margin-left: -0.6em;
-	padding: 0.35em;
+	padding: 0.125em 0.35em;
 	padding-right: 0.2em;
 	opacity: 0.35;
 	cursor: pointer;
