49411a5376b2d6e71be20bced81c63291a197d4e
Nolan Reed <jasonnolanreed@gmail.com>
2023-02-20 08:28:38 -0600

    Match position names/order between...
    ...session measures and timegrapher results

--- utilities/position.js
+++ utilities/position.js
@@ -3,10 +3,10 @@ export const positionsMap = {
 	worn: {label: `Worn`, icon: `accessibility_new`},
 	dialup: {label: `Dial Up`, icon: `arrow_upward`},
 	dialdown: {label: `Dial Down`, icon: `arrow_downward`},
-	crownup: {label: `Crown Up`, icon: `text_rotate_up`},
+	twelveup: {label: `Crown Right`, icon: `text_rotate_vertical`},
 	crowndown: {label: `Crown Down`, icon: `text_rotation_down`},
-	twelveup: {label: `12 O'Clock Up`, icon: `text_rotate_vertical`},
-	sixup: {label: `6 O'Clock Up`, icon: `text_rotate_upside_down`},
+	sixup: {label: `Crown Left`, icon: `text_rotate_upside_down`},
+	crownup: {label: `Crown Up`, icon: `text_rotate_up`},
 	winder: {label: `Winder`, icon: `rotate_right`}
 };
 
